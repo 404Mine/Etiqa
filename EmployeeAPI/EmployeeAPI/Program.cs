cbdb1bd642536b52e7d2011b5a1a6654d06941a4
Mine Encallado <michaelencallado03@gmail.com>
2025-11-30 16:45:41 +0900

    Single click to run both backend and frontend + few bug fix due to http and https mismatched after applying same runtime + removed swagger launch for backend

--- EmployeeAPI/EmployeeAPI/Program.cs
+++ EmployeeAPI/EmployeeAPI/Program.cs
@@ -7,8 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
 //manually added
 builder.Services.AddScoped<EmployeeService>();
 builder.Services.AddDbContext<EmployeeDbContext>(options =>
@@ -28,11 +28,11 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
 
 app.UseHttpsRedirection();
 
